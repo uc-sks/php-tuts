96b99e9f6879f6d5c4bc83fe5717ebaf9fdb7dfa
uc-sks <shivam.singh@ucertify.com>
2022-03-29 08:29:44 +0530

    comment.php

---
+++ comment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+// this is single line comment.
+/*
+this is 
+a 
+multiple
+line 
+comment
+*/
+echo "comment in php";
+?>
+
+</body>
+</html>
