d2c303f5b1103b258668317c8d870855355a4672
uc-sks <shivam.singh@ucertify.com>
2022-03-29 09:10:54 +0530

    logical statement

---
+++ logicalStatement.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//if
+$t = date("H");
+var_dump ($t);
+if ($t < "20") {
+  echo "Have a good day!";
+}
+// if else
+$t = date("H");
+if ($t > "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+// elseif
+$t = date("H");
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+//switch case
+$favcolor = "red";
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+?>
+</body>
+</html>
